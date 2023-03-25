de3a89fb77e1165ce7af0fb4f619666e95e2b24b
Dmytro2V <op2vin@gmail.com>
2023-03-25 23:39:30 +0200

    done all, upgraded to React18, Router v6. No bonus/css/

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import harvardArt from './data/harvardArt'
 import {Routes, Route} from 'react-router-dom';
 import GalleryNavigation from './components/GalleryNavigation';
 import GalleryView from './components/GalleryView'
-import Home from './components/Home'
 
 
 function App() {
@@ -12,7 +11,7 @@ function App() {
       <GalleryNavigation galleries={harvardArt.records}/>
       <Routes>
         <Route path="/" element='{<Home />}' />
-        <Route path='/galleries/:galleryId' element = {<GalleryView galleries={harvardArt.records}/> } />
+        <Route path='/galleries/:galleryId/*' element = {<GalleryView galleries={harvardArt.records}/> } />
         <Route path="*" element={<h2>Page Not Found</h2>} />
       </Routes>
   
